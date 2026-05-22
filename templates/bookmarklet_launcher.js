ba381eead86c0135278938f3ff5de38d6c9e4371
max <maxandreev27@gmail.com>
2026-05-22 16:54:42 +0300

    Update bookmarklet_launcher

--- templates/bookmarklet_launcher.js
+++ templates/bookmarklet_launcher.js
@@ -1,14 +1,1 @@
-(function () {
-  if (!window.bookmarklet) {
-    bookmarklet_js = document.body.appendChild(
-      document.createElement("script"),
-    );
-    // Django автоматично підставить правильний хост (локальний або продакшен)
-    bookmarklet_js.src =
-      "//{{ request.get_host }}/static/js/bookmarklet.js?r=" +
-      Math.floor(Math.random() * 9999999999999999);
-    window.bookmarklet = true;
-  } else {
-    bookmarkletLaunch();
-  }
-})();
+(function(){if(!window.bookmarklet){bookmarklet_js=document.body.appendChild(document.createElement('script'));bookmarklet_js.src='//{{ request.get_host }}/static/js/bookmarklet.js?r='+Math.floor(Math.random()*9999999999999999);window.bookmarklet=true;}else{bookmarkletLaunch();}})();
