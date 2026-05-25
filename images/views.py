5a984c44d2ff43d2bd293c993547006cf10244a5
max <maxandreev27@gmail.com>
2026-05-25 18:33:41 +0300

    Add Redis

--- images/views.py
+++ images/views.py
@@ -10,6 +10,11 @@ from actions.utils import create_action
 from .forms import ImageCreateForm
 from .models import Image
 
+# соединить с redis
+# r = redis.Redis(
+#     host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB
+# )
+
 
 @login_required
 def image_create(request):
@@ -37,8 +42,14 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    # увеличить общее число просмотров изображения на 1
+    # total_views = r.incr(f'image:{image.id}:views')
+    # увеличить рейтинг изобажения на 1
+    # r.zincrby('image_ranking', 1, image.id)
     return render(
-        request, "images/image/detail.html", {"section": "images", "image": image}
+        request,
+        "images/image/detail.html",
+        {"section": "images", "image": image},
     )
 
 
@@ -88,3 +99,18 @@ def image_list(request):
     return render(
         request, "images/image/list.html", {"section": "images", "images": images}
     )
+
+
+# @login_required
+# def image_ranking(request):
+#     # get image ranking dictionary
+#     image_ranking = r.zrange("image_ranking", 0, -1, desc=True)[:10]
+#     image_ranking_ids = [int(id) for id in image_ranking]
+#     # get most viewed images
+#     most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+#     most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+#     return render(
+#         request,
+#         "images/image/ranking.html",
+#         {"section": "images", "most_viewed": most_viewed},
+#     )
